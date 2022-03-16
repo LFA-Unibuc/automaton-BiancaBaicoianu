5036da4934fdd8dc9204d89801a8a8fbe3b89864
BiancaBaicoianu <95134928+BiancaBaicoianu@users.noreply.github.com>
2022-03-17 01:42:47 +0200

    Update automaton.py

--- automaton.py
+++ automaton.py
@@ -4,31 +4,40 @@ class Automaton():
         self.config_file = config_file
         print("Hi, I'm an automaton!")
 
-    def validate(self):
-        """Return a Boolean
+        automaton = {"sigma": [], "states": [], "transitions": []}
+        with open(self.config_file) as input:
+            while len(automaton["sigma"]) == 0 or len(automaton["stari"]) == 0 or len(automaton["tranzitii"]) == 0:
+                line = input.readline().strip()  # eliminarea spatiilor inutile
+                section = line.split(":", maxsplit=1)[0]
+                if line[0] == "#":
+                    continue
+                elif section in automaton.keys():
+                    while line != "end":
+                        line = input.readline().strip()
+                        if "end" not in line:
+                            automaton[section].append(line)  # adaugarea value in dict automaton la cheia section
 
-        Returns true if the config file is valid,
-        and raises a ValidationException if the config is invalid.
-        """
-        return "I can't tell if the config file is valid... yet!"
+        automaton["states"] = [tuple([x for x in state.split(",")]) for state in automaton["states"]]
 
-    def accepts_input(self, input_str):
-        """Return a Boolean
+        self.automaton = automaton
+        print(automaton)
 
-        Returns True if the input is accepted,
-        and it returns False if the input is rejected.
-        """
-        pass
+    def validate():
+        states = [state[1] if len(state) > 1 else "" for state in self.automaton["states"]]
+        if states.count("S") > 1:
+            print("Only one state")
+            return False
 
-    def read_input(self, input_str):
-        """Return the automaton's final configuration
-        
-        If the input is rejected, the method raises a
-        RejectionException.
-        """
-        pass
-    
+        for transition in self.automaton["transitions"]:
+            state1, word, state2 = [y for y in transition.split(",")]
+            valid_states = [state[0] for state in self.automaton["states"]]
+            if state1 not in valid_states or state2 not in valid_states or word not in self.automaton["sigma"]:
+                print("Invalid words or states")
+                return False
+        return True
+
 
 if __name__ == "__main__":
-    a = Automaton('your_config_file')
-    print(a.validate())
+    automaton = Automaton("your_config_file")
+    print(automaton.validate())
+
